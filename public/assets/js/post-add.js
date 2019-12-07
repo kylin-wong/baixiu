40be217c0334f187173c8d23627c7b7976a11e15
kylin-wong <375506637@qq.com>
2019-12-07 20:53:32 +0800

    文章修改功能

--- public/assets/js/post-add.js
+++ public/assets/js/post-add.js
@@ -46,3 +46,53 @@ $('#addForm').on('submit', function () {
     })
     return false;
 })
+
+// 获取浏览器地址栏中的id参数
+var id = getUrlParams('id')
+// 当前管理与你是在做修改文章的操作啦
+// 因为id不等于-1 所以找得id属性呀! 所以就是操作呀
+if (id != -1) {
+    $.ajax({
+        type: 'get',
+        url: '/posts/' + id,
+        success: function (response) {
+            $.ajax({
+                type: 'GET',
+                url: '/categories',
+                success: function (data) {
+                    response.data = data;
+                    console.log(response)
+                    var html = template('modifyTpl', response);
+                    $("#parentBox").html(html);
+                }
+            })
+        }
+    })
+}
+
+// 从浏览器地址栏中获取参数
+function getUrlParams(name) {
+    var paramsAry = location.search.substr(1).split('&');
+    for (var i = 0; i < paramsAry.length; i++) {
+        var tmp = paramsAry[i].split('=')
+        if (tmp[0] == name) {
+            return tmp[1];
+        }
+        // -1 在这里哦!
+        return -1;
+    }
+}
+// 当修改文章信息表单发生提交行为的时候触发
+$('#parentBox').on('submit','#modifyForm',function(e) {
+    e.preventDefault();
+    var formData = $(this).serialize();
+    var id = $(this).attr('data-id');
+    $.ajax({
+        type:'put',
+        url:'/posts/' + id,
+        data:formData,
+        success:function(data) {
+            location.href = '/admin/posts.html' 
+        }
+    })
+})
