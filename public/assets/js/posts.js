11a0254dfa71c6e8f8a3f26f59ebf5807e445e37
kylin-wong <375506637@qq.com>
2019-12-07 21:20:37 +0800

    文章删除操作

--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -1,11 +1,11 @@
 $.ajax({
-    type:'get',
-    url:'/posts',
-    success:function(data) {
+    type: 'get',
+    url: '/posts',
+    success: function (data) {
         console.log(data)
-        var html = template('postTpl',data);
+        var html = template('postTpl', data);
         $('#postsBox').html(html);
-        var page = template('pageTpl',data);
+        var page = template('pageTpl', data);
         $('#page').html(page);
     }
 })
@@ -17,43 +17,58 @@ function formateDate(date) {
 // 分页
 function changePage(page) {
     $.ajax({
-        type:'get',
-        url:'/posts',
+        type: 'get',
+        url: '/posts',
         data: {
             page: page
         },
-        success:function(data) {
+        success: function (data) {
             console.log(data)
-            var html = template('postTpl',data);
+            var html = template('postTpl', data);
             $('#postsBox').html(html);
-            var page = template('pageTpl',data);
+            var page = template('pageTpl', data);
             $('#page').html(page);
         }
     })
 }
 
 $.ajax({
-    type:'GET',
-    url:'/categories',
-    success:function(data) {
-        var html = template('tpl-category',{data:data})
+    type: 'GET',
+    url: '/categories',
+    success: function (data) {
+        var html = template('tpl-category', { data: data })
         $('#category').html(html);
     }
 })
 
-$('#filterForm').on('submit',function() {
+$('#filterForm').on('submit', function () {
     var formDate = $(this).serialize();
     $.ajax({
-        type:'get',
-        url:'/posts',
-        data:formDate,
-        success:function(data) {
+        type: 'get',
+        url: '/posts',
+        data: formDate,
+        success: function (data) {
             console.log(data)
-            var html = template('postTpl',data);
+            var html = template('postTpl', data);
             $('#postsBox').html(html);
-            var page = template('pageTpl',data);
+            var page = template('pageTpl', data);
             $('#page').html(page);
         }
     })
     return false
+})
+
+$('#postsBox').on('click', '.delete', function () {
+    if (confirm('你真的要进行删除吗?')) {
+        var id = $(this).attr('data-id');
+        $.ajax({
+            type:'delete',
+            url:'/posts/' + id,
+            success:function() {
+                location.reload();
+            }
+        })
+        
+    }
+
 })
